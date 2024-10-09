e6d58d48b03e7523bd17da9a5c6f461849d8851c
allan <allanlealluz@gmail.com>
2024-10-09 08:52:55 -0300

    09/10 1

--- src/Aula3/exemplos/Exemplos5.java
+++ src/Aula3/exemplos/Exemplos5.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 public class Exemplos5 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-
-        scanner.nextInt();
+        System.out.println("Digite um numero inteiro");
+        int numero = scanner.nextInt();
+        long varlong = numero;
+        System.out.println("Digite um numero decimal");
+        float varFloat = scanner.nextFloat();
+        System.out.println("O numero que você digitou é: "+numero);
+        System.out.println("O numero que você digitou é: "+varlong);
+        System.out.println("O numero que você digitou é: "+varFloat);
     }
 }
