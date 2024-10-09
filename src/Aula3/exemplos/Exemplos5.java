84213317fd857a9ea34131b8c8086dbfeaa9dbca
allan <allanlealluz@gmail.com>
2024-10-09 08:32:06 -0300

    scanner

---
+++ src/Aula3/exemplos/Exemplos5.java
@@ -0,0 +1,9 @@
+package Aula3.exemplos;
+import java.util.Scanner;
+public class Exemplos5 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.nextInt();
+    }
+}
