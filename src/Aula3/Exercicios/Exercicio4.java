8212fb9679180632a769b4d636d4524e237df648
Allan <allanlealluz@gmail.com>
2024-10-11 10:16:38 -0300

    6 primeiros exericicios

---
+++ src/Aula3/Exercicios/Exercicio4.java
@@ -0,0 +1,9 @@
+package Aula3.Exercicios;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        double num1 = 15.26d;
+        int num2 = (int) num1;
+        System.out.println(num1+" "+num2);
+    }
+}
