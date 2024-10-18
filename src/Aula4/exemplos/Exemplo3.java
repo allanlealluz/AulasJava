77d7fa504a13781b9538a3aa040c7145dfce7dca
Allan <allan@gmail.com>
2024-10-18 10:08:24 -0300

    exemplo3

--- src/Aula4/exemplos/Exemplo3.java
+++ src/Aula4/exemplos/Exemplo3.java
@@ -1,5 +1,22 @@
 package Aula4.exemplos;
 
-public class Exemplo3 {
+import java.util.Scanner;
 
+public class Exemplo3 {
+    public static void main(String[] args) {
+        int idade = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite sua idade");
+        idade = scanner.nextInt();
+        if(idade >0 && idade <= 12){
+            System.out.println("Você é criança ");
+        }else if(idade > 12 && idade <=17){
+            System.out.println("Você é adolescente");
+        }else if(idade > 17 && idade < 65){
+            System.out.println("Você é adulto");
+        }else{
+            System.out.println("Idoso");
+        }
+        
+    }
 }
