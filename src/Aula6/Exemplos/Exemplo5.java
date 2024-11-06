7c31b8fd4c2c6ad513ba2bc5abff5e89d0d4202d
allan <allanlealluz@gmail.com>
2024-11-06 09:00:03 -0300

    ..

--- src/Aula6/Exemplos/Exemplo5.java
+++ src/Aula6/Exemplos/Exemplo5.java
@@ -3,5 +3,10 @@ package Aula6.Exemplos;
 public class Exemplo5{
     public static void main(String[] args) {
         int[] numeros = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Posição"+i+" "+numeros[i]);
+
+        }
     }
 }
