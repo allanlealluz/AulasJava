cb9233507b03739bb8cbb8b49875bef5315b0328
allan <allanlealluz@gmail.com>
2024-11-06 08:55:39 -0300

    ..

--- src/Aula6/Exemplos/Exemplo1.java
+++ src/Aula6/Exemplos/Exemplo1.java
@@ -4,6 +4,9 @@ public class Exemplo1 {
     public static void main(String[] args) {
         int [] numeros = new int[5];
         numeros[0] = 10;
+        numeros[1] = 20;
+        numeros[2] = 30;
+        numeros[4] = 40;
         System.out.println(numeros[0]);
     }
 }
---
+++ src/Aula6/Exemplos/Exemplo5.java
@@ -0,0 +1,7 @@
+package Aula6.Exemplos;
+
+public class Exemplo5{
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 3, 4, 5};
+    }
+}
