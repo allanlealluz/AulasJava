0bacabd9308d3888ce15a3af6ba107bb8d6affce
Aluno <willianbrunomata@gmail.com>
2024-11-29 10:53:39 -0300

    ..

---
+++ src/Desafio/Desafio2.java
@@ -0,0 +1,14 @@
+package Desafio;
+
+public class Desafio2 {
+    public static void main(String[] args) {
+        String menu = """
+        |   ESCOLHA UMA OPÇÃO:     
+        |   1 - Exibir cadastro Completo    
+        |   2 - Cadastrar Usuário
+        |   3 - Atualizar Usuário
+        |   4 - Deletar Usuário 
+        |   5 - Sair           
+         """;
+    }
+}
