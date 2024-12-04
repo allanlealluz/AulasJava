1df34a8615de7b2b25b76b2ed20c7a3dbe0d8742
allan <allanlealluz@gmail.com>
2024-12-04 08:55:19 -0300

    alternate version of challenge

--- src/Desafio/Desafio2.java
+++ src/Desafio/Desafio2.java
@@ -3,8 +3,11 @@ package Desafio;
 import java.util.Scanner;
 
 public class Desafio2 {
+    static String[] cabecalho = {"ID", "NOME", "TELEFONE", "EMAIL"};
+    static String[][] matrizCadastro;
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         String menu = """
                 |   ESCOLHA UMA OPÇÃO:     
                 |   1 - Exibir cadastro Completo    
@@ -13,43 +16,108 @@ public class Desafio2 {
                 |   4 - Deletar Usuário 
                 |   5 - Sair           
                 """;
-        int opcao = scanner.nextInt();
+
+        matrizCadastro = new String[0][cabecalho.length];
+
+        int opcao;
         do {
-        System.out.println(menu);
-        opcao = scanner.nextInt();
-        scanner.nextLine();
+            System.out.println(menu);
+            opcao = scanner.nextInt();
+            scanner.nextLine();
 
             switch (opcao) {
-                case 1:
-                    exibirUsuario();
-                    break;
-                case 2:
-                    CadastrarUsuario();
-                    break;
-                case 3:
-                    atualizarUsuario();
-                    break;
-                case 4:
-                    deletarUsuario();
-                    break;
-                case 5:
-                    break;
-                default:
-                    System.out.println("Erro");
-                    break;
+                case 1 -> exibirUsuario();
+                case 2 -> cadastrarUsuario();
+                case 3 -> atualizarUsuario();
+                case 4 -> deletarUsuario();
+                case 5 -> System.out.println("Saindo...");
+                default -> System.out.println("Opção inválida. Tente novamente.");
             }
-        }while(opcao != 5);
+        } while (opcao != 5);
     }
-    public static void exibirUsuario(){
-        System.out.println("Exibir");
+
+    public static void exibirUsuario() {
+        if (matrizCadastro.length == 0) {
+            System.out.println("Nenhum usuário cadastrado.");
+            return;
+        }
+        for (String[] linha : matrizCadastro) {
+            for (String coluna : linha) {
+                System.out.print(coluna + "\t");
+            }
+            System.out.println();
+        }
     }
-    public static void CadastrarUsuario(){
-        System.out.println("Cadastar");
+
+    public static void cadastrarUsuario() {
+        System.out.println("Quantas pessoas você deseja cadastrar?");
+        int qtdPessoas = scanner.nextInt();
+        scanner.nextLine();
+        String[][] novaMatriz = new String[matrizCadastro.length + qtdPessoas][cabecalho.length];
+        for (int i = 0; i < matrizCadastro.length; i++) {
+            novaMatriz[i] = matrizCadastro[i];
+        }
+
+        // Adicionar novos usuários.
+        for (int i = matrizCadastro.length; i < novaMatriz.length; i++) {
+            novaMatriz[i] = new String[cabecalho.length];
+            novaMatriz[i][0] = String.valueOf(i); // ID.
+
+            System.out.println("Digite o nome:");
+            novaMatriz[i][1] = scanner.nextLine();
+
+            System.out.println("Digite o telefone:");
+            novaMatriz[i][2] = scanner.nextLine();
+
+            System.out.println("Digite o email:");
+            novaMatriz[i][3] = scanner.nextLine();
+        }
+
+        matrizCadastro = novaMatriz;
     }
-    public static void atualizarUsuario(){
-        System.out.println("Exibir");
+
+    public static void atualizarUsuario() {
+        System.out.println("Digite o ID do usuário que deseja atualizar:");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        if (id < 0 || id >= matrizCadastro.length) {
+            System.out.println("ID inválido.");
+            return;
+        }
+
+        System.out.println("Atualizando dados para o usuário ID " + id);
+        System.out.println("Digite o novo nome:");
+        matrizCadastro[id][1] = scanner.nextLine();
+
+        System.out.println("Digite o novo telefone:");
+        matrizCadastro[id][2] = scanner.nextLine();
+
+        System.out.println("Digite o novo email:");
+        matrizCadastro[id][3] = scanner.nextLine();
+
+        System.out.println("Usuário atualizado com sucesso.");
     }
-    public static void deletarUsuario(){
-        System.out.println("Exibir");
+
+    public static void deletarUsuario() {
+        System.out.println("Digite o ID do usuário que deseja deletar:");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        if (id < 0 || id >= matrizCadastro.length) {
+            System.out.println("ID inválido.");
+            return;
+        }
+        String[][] novaMatriz = new String[matrizCadastro.length - 1][cabecalho.length];
+
+        int novaIndex = 0;
+        for (int i = 0; i < matrizCadastro.length; i++) {
+            if (i != id) {
+                novaMatriz[novaIndex++] = matrizCadastro[i];
+            }
+        }
+
+        matrizCadastro = novaMatriz;
+        System.out.println("Usuário deletado com sucesso.");
     }
 }
